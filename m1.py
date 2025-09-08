5f17a08ff50ff0ce7b8d5710b0e2cee81e4950f7
mahek chotara <mahekchotara@gmail.com>
2025-09-08 23:56:39 +0530

    Create m1.py

---
+++ m1.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    records = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records.append([name, score])
+    
+    scores = sorted({s for _, s in records})
+    second_lowest = scores[1]
+    
+    names = sorted([name for name, score in records if score == second_lowest])
+    for name in names:
+        print(name)
