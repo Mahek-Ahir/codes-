1e807d8dc4779530320ba3a65f20ccfcaf9e7f7d
mahek chotara <mahekchotara@gmail.com>
2025-10-02 18:21:30 +0530

    Create didgiclock.py

---
+++ didgiclock.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import time
+
+def update_time():
+    current_time = time.strftime("%H:%M:%S")
+    label.config(text=current_time)
+    label.after(1000, update_time)
+
+root = tk.Tk()
+root.title("Digital Clock")
+
+label = tk.Label(root, font=("Arial", 50), bg="black", fg="cyan")
+label.pack(padx=20, pady=20)
+
+update_time()
+root.mainloop()
